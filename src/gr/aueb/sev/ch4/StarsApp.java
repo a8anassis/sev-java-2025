1392a981f9625a0959ae4574e2c3b4f23bb83435
a8anassis <a8anassis@gmail.com>
2025-10-13 19:48:09 +0300

    feat: add stars app with for

---
+++ src/gr/aueb/sev/ch4/StarsApp.java
@@ -0,0 +1,43 @@
+package gr.aueb.sev.ch4;
+
+/**
+ * Prints 10 horizontal stars.
+ */
+public class StarsApp {
+
+    public static void main(String[] args) {
+
+        // Prints 10 horizontal stars
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("*");
+        }
+
+        // Prints 10 vertical stars
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("*");
+        }
+
+        // Prints 10 rows with 10 stars in each row
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Prints 1, 2, 3, 4, ... 10
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 10; i >= 1; i--){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
