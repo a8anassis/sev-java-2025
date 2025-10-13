939bd4c7e486cf48025aef77eeb8b6ff861fd0e7
a8anassis <a8anassis@gmail.com>
2025-10-13 19:50:51 +0300

    fix: add alternative case

--- src/gr/aueb/sev/ch4/StarsApp.java
+++ src/gr/aueb/sev/ch4/StarsApp.java
@@ -39,5 +39,12 @@ public class StarsApp {
             }
             System.out.println();
         }
+
+        for (int i = 1; i <= 10; i++){
+            for (int j = i; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
