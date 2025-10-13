1d25cf566d9ca6cc3600e5f1936124d886ce147c
a8anassis <a8anassis@gmail.com>
2025-10-13 20:07:35 +0300

    fix: add floating point examples

---
+++ src/gr/aueb/sev/ch5/DeclarationsApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.sev.ch5;
+
+public class DeclarationsApp {
+
+    public static void main(String[] args) {
+        float myFloat = 3.5F;
+        double myDouble = 3.5;
+        final double PI = 3.14;
+
+    }
+}
---
+++ src/gr/aueb/sev/ch5/EpsilonApp.java
@@ -0,0 +1,24 @@
+package gr.aueb.sev.ch5;
+
+/**
+ * Σωστή σύγκριση δεκαδικών. Όχι, με == !
+ */
+public class EpsilonApp {
+
+    public static void main(String[] args) {
+        final double EPSILON = 0.000005; // 6 significant digits
+        double actual = 0.0;
+        double expected = 1.0;
+
+        for (int i = 1; i <= 10; i++) {
+            actual = actual + 0.1;
+        }
+
+        System.out.println(actual);
+        if (Math.abs(actual - expected) <= EPSILON) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+    }
+}
