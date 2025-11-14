546ff26aee709508a42ea5d99c8be3b688f43002
a8anassis <a8anassis@gmail.com>
2025-11-14 20:16:49 +0200

    fix: add examples

---
+++ src/gr/aueb/sev/ch14/Main.java
@@ -0,0 +1,16 @@
+package gr.aueb.sev.ch14;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p1 = Point.getInstance();
+        Point p2 = Point.getRandomInstance();
+
+        CodingFactory codingFactory = CodingFactory.getInstance();
+        CodingFactory codingFactory2 = CodingFactory.getInstance();
+
+        codingFactory.sayHello();
+        codingFactory2.sayHello();
+
+    }
+}
