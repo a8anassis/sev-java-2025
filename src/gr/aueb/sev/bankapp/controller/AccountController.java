cd13c9c3a1ff32daf95de03c9fb18946fa6a9278
a8anassis <a8anassis@gmail.com>
2026-01-08 19:34:28 +0200

    feat: add new methods to controller

--- src/gr/aueb/sev/bankapp/controller/AccountController.java
+++ src/gr/aueb/sev/bankapp/controller/AccountController.java
@@ -1,13 +1,18 @@
 package gr.aueb.sev.bankapp.controller;
 
+import gr.aueb.sev.bankapp.core.exceptions.AccountNotFoundException;
+import gr.aueb.sev.bankapp.core.exceptions.InsufficientBalanceException;
 import gr.aueb.sev.bankapp.core.exceptions.ValidationException;
+import gr.aueb.sev.bankapp.dto.AccountDepositDTO;
 import gr.aueb.sev.bankapp.dto.AccountInsertDTO;
 import gr.aueb.sev.bankapp.dto.AccountReadOnlyDTO;
+import gr.aueb.sev.bankapp.dto.AccountWithdrawDTO;
 import gr.aueb.sev.bankapp.model.Account;
 import gr.aueb.sev.bankapp.service.IAccountService;
 import gr.aueb.sev.bankapp.validation.Validator;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 public class AccountController {
@@ -35,5 +40,54 @@ public class AccountController {
         // Call service
         accountReadOnlyDTO = accountService.createNewAccount(accountInsertDTO);
         return accountReadOnlyDTO;
+    }
+
+    public void deposit(String iban, BigDecimal amount)
+            throws AccountNotFoundException, ValidationException {
+
+        AccountDepositDTO accountDepositDTO = new AccountDepositDTO(iban, amount);
+
+        // Validation
+        Map<String, String> errors;
+        errors = Validator.validateDepositDTO(accountDepositDTO);
+
+        if (!errors.isEmpty()) {
+            throw new ValidationException(errors.toString());
+        }
+
+        accountService.deposit(accountDepositDTO);
+    }
+
+    public void withdraw(String iban, BigDecimal amount)
+            throws AccountNotFoundException, ValidationException, InsufficientBalanceException {
+
+        AccountWithdrawDTO withdrawDTO = new AccountWithdrawDTO(iban, amount);
+
+        // Validation
+        Map<String, String> errors;
+        errors = Validator.validateWithdrawDTO(withdrawDTO);
+
+        if (!errors.isEmpty()) {
+            throw new ValidationException(errors.toString());
+        }
+
+        // Call service
+        accountService.withdraw(withdrawDTO);
+    }
+
+    public BigDecimal getBalance(String iban) throws AccountNotFoundException {
+        return accountService.getBalance(iban);
+    }
+
+    public List<AccountReadOnlyDTO> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    public int getCount() {
+        return accountService.getCount();
+    }
+
+    public boolean isAccountExists(String iban) {
+        return accountService.isAccountExists(iban);
     }
 }
